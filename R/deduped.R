83677777e197b827acad70e8847c06ac7a214492
orgadish <48453207+orgadish@users.noreply.github.com>
2023-10-09 21:21:44 -0700

    Update dedup example to reduce duration 10x

--- R/deduped.R
+++ R/deduped.R
@@ -10,14 +10,15 @@
 #' x <- sample(LETTERS, 10)
 #' x
 #'
-#' large_x <- sample(rep(x, 100))
+#' large_x <- sample(rep(x, 10))
 #' length(large_x)
 #'
 #' long_func <- function(x) for(i in x) {Sys.sleep(0.001)}
 #'
-#' system.time({y <- long_func(large_x)})
+#' system.time({y1 <- long_func(large_x)})
 #' system.time({y2 <- deduped(long_func)(large_x)})
-#' all(y == y2)
+#'
+#' all(y1 == y2)
 deduped <- function(f) {
   function(x, ...) {
     ux <- collapse::funique(x)
