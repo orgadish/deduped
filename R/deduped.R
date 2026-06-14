a91babd1f8a10f3dd4d53719016b81e60f8932bf
orgadish <48453207+orgadish@users.noreply.github.com>
2026-06-13 23:46:38 -0700

    Add early exits to deduped when trivially short, or no duplication and improve error messaging.

--- R/deduped.R
+++ R/deduped.R
@@ -44,35 +44,53 @@ deduped <- function(f) {
 
   function(x, ...) {
 
+    # If x is trivially short, skip deduplication overhead.
+    if (length(x) <= 1L)
+      return(f(x, ...))
+
     # collapse::funique() and collapse::fmatch() are faster than the base
     # equivalents, but behave differently on lists.
     if (inherits(x, "list")) {
       ux <- unique(x)
-      uf <- f(ux, ...)
-      out <- uf[match(x, ux)]
+      match_fn <- match
     }
 
     # Deduped only works on atomic vectors or lists, but using is.vector() is
     # too restrictive since it fails on a vector with attributes. Instead we
     # just exclude matrices and arrays.
     else if (is.atomic(x) && is.null(dim(x))) {
-
       ux <- collapse::funique(x)
-      uf <- f(ux, ...)
-      out <- uf[collapse::fmatch(x, ux)]
-    } else {
-      warning(paste(
-        "`deduped(f)(x)` only works on atomic vectors or list inputs.",
-        "Proceeding with f(x, ...) directly."
-      ))
+      match_fn <- collapse::fmatch
+    }
+
+    else {
+      warning(
+        "`deduped(f)(x)` only works on atomic vectors or list inputs.\n",
+        "Proceeding with f(x, ...) directly.",
+        call. = FALSE
+      )
       return(f(x, ...))
     }
 
+
+    # If there is no duplication, avoid the re-expansion overhead by calling
+    # on the original values to minimize the case where `ux` is any different,
+    # e.g. if attributes changed.
+    if(length(ux) == length(x))
+      return(f(x, ...))
+
+    uf <- f(ux, ...)
+
     # Check for functions that reduce length by comparing ux/ug.
     # There are some cases that this won't catch, but uf[match(x, ux)] results
-    # in NAs which mean we can't check the length directly.
+    # in NAs which mean we can't check the final length directly.
     if(length(uf) != length(ux))
-      stop("deduped only works with functions that preserve length.")
+      stop(sprintf(
+          "deduped() requires a length-preserving function, but f reduced %d unique value(s) to %d.",
+        length(ux), length(uf)
+      ))
+
+    out <- uf[match_fn(x, ux)]
 
 
     # Since we ensure length is the same, keep attributes.
